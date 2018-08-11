0dbc3cd4f9d55241f52e210e45512ae4e93935e3
pasha4uu <pasha7864u@gmail.com>
2018-08-11 15:44:13 +0530

    first commit

--- CalulateHumanAge/ViewController.swift
+++ CalulateHumanAge/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var dogAgeTF: UITextField!
+    
+    @IBOutlet weak var ageLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func calculateHumanAge(_ sender: Any) {
+        
+        if dogAgeTF.text != "" {
+            
+            let dogAge = Int(dogAgeTF.text!)
+            
+            ageLbl.text = "Human age is : \(calculateAge(dogAge: dogAge!))"
+        }
+      else
+        {
+          ageLbl.text = "Please enter Dog Age"
+        }
+        
+    }
+    
+    func calculateAge(dogAge:Int) -> Int {
+        
+        var humanAge = 0
+        
+        
+        if dogAge == 1{
+            humanAge = 15
+        }
+        else if dogAge == 2 {
+            
+            humanAge = 24
+        }
+        else {
+            
+            humanAge = ((dogAge - 2) * 4) + 24
+            
+        }
+        return humanAge
+    }
 
 }
 
